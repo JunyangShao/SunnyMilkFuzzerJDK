3d42e58eb18e23bead3f693432eb39befa30c6f6
JunyangShao <351775009@qq.com>
2023-06-14 11:42:18 -0400

    update benchmark scripts

--- benchmark/fuzzer-finder/run_fuzzers.py
+++ benchmark/fuzzer-finder/run_fuzzers.py
@@ -1,6 +1,19 @@
 import os
 import subprocess
 import sys
+import re
+
+def parse_file(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    pattern = r"#(\d+)\t"
+    for line in reversed(lines):
+        match = re.search(pattern, line)
+        if match:
+            return int(match.group(1))
+
+    return 0
 
 # image names
 image_names = [
@@ -15,10 +28,11 @@ image_names = [
 image_names_excluded = ["jersey","mysql-connector-j", "guice", "self4j-api",
                         "spring-cloud-sleuth-brave", "java-example", "json-sanitizer",
                         "osgi", "snakeyaml", "spring-data-mongodb", "log4j2",
-                        "retrofit", "spring-boot"]
+                        "retrofit", "spring-boot", "slf4j-api"]
 
 mode = "print"
 running_time = 10
+suffix = ""
 # mode 'all' means run fuzzers and analyze benchmark reports.
 # mode 'benchmark' means only analyze benchmark reports.
 # mode 'fuzzers' means only run fuzzers.
@@ -28,6 +42,10 @@ if len(sys.argv) > 1:
     if mode == "all" or mode == "fuzzers":
         if len(sys.argv) > 2:
             running_time = int(sys.argv[2])
+    if mode == "benchmark":
+        if len(sys.argv) > 2:
+            suffix = sys.argv[2]
+            
 
 if mode == "print":
     print(set(image_names) - set(image_names_excluded))
@@ -75,8 +93,8 @@ if mode == "all" or mode == "fuzzers":
             os.system(smf_cmd)
 
 # parse all the reports, they are in ../extracted/{image_name}/{fuzzer}[Jazzer|SMF]Report
-print("{:<30}{:<30}{:<30}{:<30}{:<30}".format("Project Name", "Fuzzer", "Jazzer Insts", "SMF Insts", "Percentage (SMF/Jazzer)"))
 if mode == "all" or mode == "benchmark":
+    print("{:<20}{:<30}{:<20}{:<20}{:<20}{:<20}".format("Project Name", "Fuzzer", "Jazzer Insts", "SMF Insts", "Cov", "Thruput"))
     for image_name in image_names:
         if image_name in image_names_excluded:
             continue
@@ -86,15 +104,43 @@ if mode == "all" or mode == "benchmark":
                 fuzzer_list.add(fuzzer.replace("SMF.java", "").replace("Jazzer.java", "").replace("Main.java", ""))
         for fuzzer in fuzzer_list:
             jazzer_stats = int(subprocess.run(["python", "parse_jacoco_report.py",\
-                                    f"../extracted/{image_name}/{fuzzer}JazzerReport/index.html"],\
+                                    f"../extracted/{image_name}/{fuzzer}JazzerReport{suffix}/index.html"],\
                                     check=True, text=True, stdout=subprocess.PIPE).stdout.strip())
             smf_stats = int(subprocess.run(["python", "parse_jacoco_report.py",\
-                                    f"../extracted/{image_name}/{fuzzer}SMFReport/index.html"],\
+                                    f"../extracted/{image_name}/{fuzzer}SMFReport{suffix}/index.html"],\
                                     check=True, text=True, stdout=subprocess.PIPE).stdout.strip())
-            if (jazzer_stats == 0):
-                print("{:<30}{:<30}{:<30}{:<30}{:<30}".format(image_name, fuzzer, jazzer_stats, smf_stats, "N/A"))
+            jazzer_cnts = parse_file(f"../extracted/{image_name}/{fuzzer}JazzerReport{suffix}/jazzer-out")
+            smf_cnts = parse_file(f"../extracted/{image_name}/{fuzzer}SMFReport{suffix}/smf-out")
+            if (jazzer_stats == 0 and jazzer_cnts == 0):
+                print("{:<20}{:<30}{:<20}{:<20}{:<20}{:<20}".format(image_name, fuzzer, jazzer_stats, smf_stats, "N/A", "N/A"))
+            elif (jazzer_stats == 0):
+                formatted_percentage_inst = "{:.2f}%".format((float(smf_stats)/jazzer_stats) * 100)
+                print("{:<20}{:<30}{:<20}{:<20}{:<20}{:<20}".format(image_name, fuzzer, jazzer_stats, smf_stats,\
+                                                                    "N/A",\
+                                                                    formatted_percentage_inst))
+            elif (jazzer_cnts == 0):
+                formatted_percentage_thruput = "{:.2f}%".format((float(smf_cnts)/jazzer_cnts) * 100)
+                print("{:<20}{:<30}{:<20}{:<20}{:<20}{:<20}".format(image_name, fuzzer, jazzer_stats, smf_stats,\
+                                                                    formatted_percentage_thruput,\
+                                                                    "N/A"))
             else:
-                formatted_percentage = "{:.2f}%".format((float(smf_stats)/jazzer_stats) * 100)
-                print("{:<30}{:<30}{:<30}{:<30}{:<30}".format(image_name, fuzzer, jazzer_stats, smf_stats, formatted_percentage))
-            
-            
+                formatted_percentage_thruput = "{:.2f}%".format((float(smf_cnts)/jazzer_cnts) * 100)
+                formatted_percentage_inst = "{:.2f}%".format((float(smf_stats)/jazzer_stats) * 100)
+                print("{:<20}{:<30}{:<20}{:<20}{:<20}{:<20}".format(image_name, fuzzer, jazzer_stats, smf_stats,\
+                                                                    formatted_percentage_inst,\
+                                                                    formatted_percentage_thruput))          
+
+if mode == "backup":
+    suffix = "old"
+    if len(sys.argv) > 2:
+        suffix = sys.argv[2]
+    for image_name in image_names:
+        if image_name in image_names_excluded:
+            continue
+        fuzzer_list = set()
+        for fuzzer in os.listdir(f"../extracted/{image_name}"):
+            if fuzzer.endswith(".java"):
+                fuzzer_list.add(fuzzer.replace("SMF.java", "").replace("Jazzer.java", "").replace("Main.java", ""))
+        for fuzzer in fuzzer_list:
+            os.system(f"mv ../extracted/{image_name}/{fuzzer}JazzerReport/ ../extracted/{image_name}/{fuzzer}JazzerReport{suffix}")
+            os.system(f"mv ../extracted/{image_name}/{fuzzer}SMFReport/ ../extracted/{image_name}/{fuzzer}SMFReport{suffix}")
