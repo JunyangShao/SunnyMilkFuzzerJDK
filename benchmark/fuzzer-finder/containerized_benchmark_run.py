85cdedddde4bdd0cbc593af6d1dd8ffd0ac91e11
JunyangShao <351775009@qq.com>
2023-08-19 15:08:41 -0400

    fix bugs in the script

--- benchmark/fuzzer-finder/containerized_benchmark_run.py
+++ benchmark/fuzzer-finder/containerized_benchmark_run.py
@@ -5,6 +5,10 @@ def run_cmd(cmd):
     """Utility function to run a shell command and return its output."""
     return subprocess.check_output(cmd, stderr=subprocess.STDOUT).decode("utf-8")
 
+def run_cmd_unchecked(cmd):
+    """Utility function to run a shell command and return its output."""
+    return subprocess.run(cmd, check=False, stderr=subprocess.STDOUT)
+
 # Get the list of docker images
 cmd = ["docker", "image", "ls", "--format", "{{.Repository}}"]
 images = run_cmd(cmd).splitlines()
@@ -28,20 +32,26 @@ for image in filtered_images:
             filepath = f"/out/{fuzzer}"
             
             # Replace --trace=none with the new content inside the Docker container using sed
-            replace_cmd = f"sed -i 's|--trace=none|{fuzzer}Corpus --trace=none -max_total_time=60 --coverage_report={fuzzer}CovReport|' {filepath}"
+            replace_cmd = f"sed -i 's|--trace=none|{fuzzer}Corpus --trace=none --coverage_report={fuzzer}CovReport|' {filepath}"
+            run_cmd(["docker", "exec", container_id, "bash", "-c", replace_cmd])
+            replace_cmd = f"sed -i 's|/out/jazzer-smf-bin/jazzer|timeout 60 /out/jazzer-smf-bin/jazzer|' {filepath}"
+            run_cmd(["docker", "exec", container_id, "bash", "-c", replace_cmd])
+            replace_cmd = f"sed -i 's|/out/jazzer-orig-bin/jazzer|timeout 60 /out/jazzer-orig-bin/jazzer|' {filepath}"
             run_cmd(["docker", "exec", container_id, "bash", "-c", replace_cmd])
 
             # Execute the modified fuzzer file inside the container and redirect outputs
-            execute_cmd = f"cd /out/ && mkdir {fuzzer}Corpus && ./{fuzzer} > {fuzzer}Output 2>&1"
-            run_cmd(["docker", "exec", container_id, "bash", "-c", execute_cmd])
+            execute_cmd = f"cd /out/ && mkdir -p {fuzzer}Corpus && ./{fuzzer} > {fuzzer}Output 2>&1"
+            print(f"Executing {execute_cmd} for {image}...")
+            run_cmd_unchecked(["docker", "exec", container_id, "bash", "-c", execute_cmd])
     
     finally:
         # Copy outputs and reports from the container to the local directory
         for fuzzer in fuzzers:
             # Ensure the target directory exists
-            local_dir = f"../containerOut/{fuzzer}Results"
+            local_dir = f"../containerOut/{image}/{fuzzer}Results"
             os.makedirs(local_dir, exist_ok=True)
 
+            filepath = f"/out/{fuzzer}"
             run_cmd(["docker", "cp", f"{container_id}:{filepath}Output", f"{local_dir}/{fuzzer}Output"])
             run_cmd(["docker", "cp", f"{container_id}:{filepath}CovReport", f"{local_dir}/{fuzzer}CovReport"])
             run_cmd(["docker", "cp", f"{container_id}:{filepath}Corpus", f"{local_dir}/{fuzzer}Corpus"])
